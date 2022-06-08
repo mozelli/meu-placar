34e33a49242f578f71e3e1061de0217ad1010a94
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-06-08 13:16:51 -0300

    create layouts folder and Main layout

---
+++ src/layouts/Main/main.jsx
@@ -0,0 +1,11 @@
+import styles from './main.module.scss';
+
+const Main = () => {
+  return (
+    <div className={ styles.main }>
+      <h1>Meu Placar</h1>
+    </div>
+  );
+}
+
+export default Main;
